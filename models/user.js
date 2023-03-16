616df2a0e648eb0d7c09d98122578fdc685a13ed
Ebadurrehman <ebadurrehman2012@gmail.com>
2023-03-16 15:26:53 -0400

    test

--- models/user.js
+++ models/user.js
@@ -19,7 +19,7 @@ mongoose.connect('mongodb+srv://ebadurrehman:Iba22395@fyp.sphtxvo.mongodb.net/ro
   .catch((error) => {
     console.log('Connection failed.');
   });
-
+  const validDomains = ['iba.edu.pk', 'khi.iba.edu.pk'];
 // Signup API
 router.post('/signup', async (req, res) => {
   const { username, email, password,erp } = req.body;
@@ -35,16 +35,22 @@ router.post('/signup', async (req, res) => {
   try {
     const existingUser = await User.findOne({ email: req.body.email });
     const existingUsererp = await User.findOne({ erp: req.body.erp });
-
+    const domain = email.split('@')[1];
+    if (!email || !password || !username)
+      return res.status(400).json({ msg: "Not all fields have been entered." });
+    if (!validDomains.includes(domain)) {
+      return res.status(400).send('Invalid email domain');
+    }
     if (existingUser) {
       return res.status(400).json({ message: 'User already exists' });
     }
     else if (existingUsererp) {
-      return res.status(400).json({ message: 'User already exists' });
+      return res.status(400).json({ message: 'erp already exists' });
     }
     // Save the user object to the database
     await user.save();
     res.status(201).json({ message: 'User created successfully.' });
+    //console.log(user._id)
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: 'An error occurred.' });
@@ -68,5 +74,26 @@ router.post('/signin', async (req, res) => {
     res.status(500).json({ message: 'Server error' });
   }
 });
+//edit user
+router.put('/users/:userId', async (req, res) => {
+  const userId = req.params.userId;
+  //console.log(userId)
+  const { name,password} = req.body;
+try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
 
+    user.username = name || user.username;
+    user.password = password || user.password;
+
+    await user.save();
+
+    res.json({ message: 'User updated successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
 module.exports = router;
