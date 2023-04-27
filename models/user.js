7f1c8222bf637f5d427b8933d984e34d5fc2dda3
affan-ali-khan <116781072+affan-ali-khan@users.noreply.github.com>
2023-04-27 05:25:03 +0500

    return userid in signup

--- models/user.js
+++ models/user.js
@@ -128,7 +128,7 @@ router.post('/signin', async (req, res) => {
         expiresIn: "1h"
       },
       );
-      res.status(200).json({ message: 'Sign in successful', token: token });
+      res.status(200).json({ message: 'Sign in successful', token: token, user: user._id });
     }
   } catch (err) {
     console.error(err);
